565a1c4bd823a6b69d8d9a95697c9fc06644b805
Agustin <Agustin@DESKTOP-5PIRN4I>
2020-08-28 14:22:36 +0200

    Addes two Scenes
    Added Main Menu Scene + MapsScene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,20 +4,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void SituarCooperativa()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
-    public void btnChangeSceneClicked() 
-    {
-        SceneManager.LoadScene(1,LoadSceneMode.Single);
-    }
 }
